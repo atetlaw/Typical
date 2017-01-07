3c611bbb2cc60ebc61ad45d64064f853e3c6426d
Andrew <andrew@tetlaw.id.au>
2017-01-07 21:39:33 +0800

    Initial Commit

---
+++ Typical/Typical.h
@@ -0,0 +1,19 @@
+//
+//  Typical.h
+//  Typical
+//
+//  Created by Andrew Tetlaw on 7/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Typical.
+FOUNDATION_EXPORT double TypicalVersionNumber;
+
+//! Project version string for Typical.
+FOUNDATION_EXPORT const unsigned char TypicalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Typical/PublicHeader.h>
+
+
